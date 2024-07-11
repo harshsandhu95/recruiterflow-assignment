142353b45ac778bfb6e255314cd1a77aba8cfdd5
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-11 18:33:02 +0530

    Components: Create World Business Section

--- src/routes/Home/WorldBusiness/_components/Article.tsx
+++ src/routes/Home/WorldBusiness/_components/Article.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/Button";
-import { formatDateTime } from "@/lib/utils";
-import type { BusinessNews } from "@/types/BusinessNews";
 import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/Button";
+import { cn, formatDateTime } from "@/lib/utils";
+import type { BusinessNews } from "@/types/BusinessNews";
 
 interface Props {
   article: BusinessNews;
@@ -11,29 +11,41 @@ export default function Article({ article }: Props) {
   const coverImg = article.multimedia.find(
     ({ format }) => format === "mediumThreeByTwo440",
   );
-  const { date, time, day } = formatDateTime(article.published_date);
+  const { date, day } = formatDateTime(article.published_date);
 
   return (
-    <article className="p-6 space-y-4 bg-card text-card-foreground shadow rounded-lg">
-      <div className="flex items-center justify-between gap-4 text-xs">
-        <p>
-          {day}, {date}
-        </p>
-
-        <p>{time}</p>
+    <article className="p-6 flex flex-col items-start justify-between space-y-4 bg-card text-card-foreground shadow-md rounded-lg">
+      <div className="w-full flex items-center justify-between gap-4 text-xs">
+        <p>{day}</p>
+        <p>{date}</p>
       </div>
 
-      <img
-        src={coverImg?.url}
-        alt={coverImg?.copyright}
-        className="w-full object-cover rounded-lg"
-      />
+      {coverImg ? (
+        <img
+          src={coverImg?.url}
+          alt={coverImg?.copyright}
+          className="flex-1 w-full object-cover rounded-lg aspect-[16/9]"
+        />
+      ) : (
+        <div className="flex items-center justify-center w-full bg-muted text-muted-foreground rounded-lg aspect-[16/9]">
+          <p className="text-xs">No image found!</p>
+        </div>
+      )}
 
-      <h3 className="text-xl font-bold line-clamp-1">{article.title}</h3>
-      <p className="line-clamp-3">{article.abstract}</p>
+      <div className="flex-1 space-y-4">
+        <h3
+          className={cn([
+            "text-xl font-bold",
+            article.abstract && "line-clamp-1",
+          ])}
+        >
+          {article.title}
+        </h3>
+        {article.abstract && <p className="line-clamp-3">{article.abstract}</p>}
+      </div>
 
-      <Button asChild>
-        <Link to="/">Read More...</Link>
+      <Button asChild variant="link" size="link">
+        <Link to={`/business/${article.slug_name}`}>Read More</Link>
       </Button>
     </article>
   );
--- src/routes/Home/WorldBusiness/index.tsx
+++ src/routes/Home/WorldBusiness/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Article from "./_components/Article";
 import { useGetBusinessNewsQuery } from "@/services/newsApi";
+import { Skeleton } from "@/components/ui/Skeleton";
 
 export default function WorldBusiness() {
-  const { data } = useGetBusinessNewsQuery();
+  const { data, isLoading } = useGetBusinessNewsQuery();
   const articles = React.useMemo(() => (data ? data.results : []), [data]);
+
+  const skeletonArray = Array.from({ length: 20 }, (_, index) => (
+    <Skeleton key={index} className="flex-1 w-full" />
+  ));
 
   return (
     <section className="space-y-4">
       <h1 className="text-xl font-bold">World Business News</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {isLoading && skeletonArray}
         {articles.map((article) => (
           <Article key={article.slug_name} article={article} />
         ))}
