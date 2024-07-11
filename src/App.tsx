c2da94cd846af950f5599931ac811e9d3ce8f95e
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-11 15:23:20 +0530

    App: Wrap App in Redux Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 import rootRouter from "@/routes";
+import { store } from "@/store";
 
 function App() {
-  return <RouterProvider router={rootRouter} />;
+  return (
+    <ReduxProvider store={store}>
+      <RouterProvider router={rootRouter} />
+    </ReduxProvider>
+  );
 }
 
 export default App;
