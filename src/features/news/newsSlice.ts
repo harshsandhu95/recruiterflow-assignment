7305a9aa336f503400000d08cadb698f4830aa1a
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-12 02:36:59 +0530

    Store: Update newsSlice

--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -1,32 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { formatISO } from "date-fns";
 
-export interface NewsState {
-  id?: string;
+export interface News {
+  id: string;
   title: string;
-  description: string;
+  body: string;
   dateTime?: string;
   deleted?: boolean;
 }
 
-const initialState: NewsState[] = [];
+const localStorageKey = "news";
+const localNews = localStorage.getItem(localStorageKey);
+
+const initialState: News[] = localNews !== null ? JSON.parse(localNews) : [];
 
 export const newsSlice = createSlice({
   name: "News",
   initialState,
   reducers: {
-    addNews(state, actions: PayloadAction<NewsState>) {
+    setNews(state, actions: PayloadAction<News[]>) {
+      return (state = actions.payload);
+    },
+    addNews(state, actions: PayloadAction<News>) {
       state.push({
         dateTime: formatISO(new Date()),
         deleted: false,
         ...actions.payload,
       });
+
+      localStorage.setItem(localStorageKey, JSON.stringify(state));
     },
     removeNews(state, actions: PayloadAction<string>) {
-      state.findIndex((news) => news.id === actions.payload);
+      const newArr = state.filter((item) => item.id !== actions.payload);
+      localStorage.setItem(localStorageKey, JSON.stringify(newArr));
+      return newArr;
     },
   },
 });
 
-export const { addNews, removeNews } = newsSlice.actions;
+export const { setNews, addNews, removeNews } = newsSlice.actions;
 export default newsSlice.reducer;
