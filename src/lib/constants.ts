b3983761e1babdf80b1fbeceb52941b8b1be223d
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-11 14:08:27 +0530

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,84 @@
+export const data = [
+  {
+    slug_name: "10archegos-verdict",
+    section: "Business",
+    subsection: "",
+    title:
+      "Jury Finds Archegos Founder Bill Hwang Guilty of Fraud and Racketeering",
+    abstract:
+      "The 2021 collapse of Archegos Capital Management led to some $10 billion in losses for some Wall Street banks. He could spend the rest of his life in prison.",
+    uri: "nyt://article/c5a7d3fb-d135-5001-a505-7c6a40605f5e",
+    url: "https://www.nytimes.com/2024/07/10/business/archegos-bill-hwang-guilty.html",
+    byline: "By Matthew Goldstein",
+    item_type: "Article",
+    source: "New York Times",
+    updated_date: "2024-07-10T20:23:55-04:00",
+    created_date: "2024-07-10T16:56:43-04:00",
+    published_date: "2024-07-10T16:56:43-04:00",
+    first_published_date: "2024-07-10T16:56:43-04:00",
+    material_type_facet: "News",
+    kicker: "",
+    subheadline: "",
+    des_facet: [
+      "Securities and Commodities Violations",
+      "Banking and Financial Institutions",
+      "Decisions and Verdicts",
+      "Jury System",
+      "Frauds and Swindling",
+    ],
+    org_facet: ["Archegos Capital Management LLC"],
+    per_facet: [
+      "Hwang, Bill",
+      "Hellerstein, Alvin K",
+      "Halligan, Patrick (Financial Executive)",
+    ],
+    geo_facet: [],
+    related_urls: [],
+    multimedia: [
+      {
+        url: "https://static01.nyt.com/images/2024/07/10/multimedia/10archegos-trial-gvtf/10archegos-trial-gvtf-thumbStandard.jpg",
+        format: "Standard Thumbnail",
+        height: 75,
+        width: 75,
+        type: "image",
+        subtype: "photo",
+        caption:
+          "The sudden collapse in 2021 of Archegos Capital Management caused steep losses for the banks that had facilitated his firm’s trading.",
+        copyright: "Jefferson Siegel for The New York Times",
+      },
+      {
+        url: "https://static01.nyt.com/images/2024/07/10/multimedia/10archegos-trial-gvtf/10archegos-trial-gvtf-mediumThreeByTwo210.jpg",
+        format: "mediumThreeByTwo210",
+        height: 140,
+        width: 210,
+        type: "image",
+        subtype: "photo",
+        caption:
+          "The sudden collapse in 2021 of Archegos Capital Management caused steep losses for the banks that had facilitated his firm’s trading.",
+        copyright: "Jefferson Siegel for The New York Times",
+      },
+      {
+        url: "https://static01.nyt.com/images/2024/07/10/multimedia/10archegos-trial-gvtf/10archegos-trial-gvtf-mediumThreeByTwo440.jpg",
+        format: "mediumThreeByTwo440",
+        height: 293,
+        width: 440,
+        type: "image",
+        subtype: "photo",
+        caption:
+          "The sudden collapse in 2021 of Archegos Capital Management caused steep losses for the banks that had facilitated his firm’s trading.",
+        copyright: "Jefferson Siegel for The New York Times",
+      },
+      {
+        url: "https://static01.nyt.com/images/2024/07/10/multimedia/10archegos-trial-gvtf/10archegos-trial-gvtf-articleInline.jpg",
+        format: "Normal",
+        height: 127,
+        width: 190,
+        type: "image",
+        subtype: "photo",
+        caption:
+          "The sudden collapse in 2021 of Archegos Capital Management caused steep losses for the banks that had facilitated his firm’s trading.",
+        copyright: "Jefferson Siegel for The New York Times",
+      },
+    ],
+  },
+];
