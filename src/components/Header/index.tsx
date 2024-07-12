fa6bd7823220c64616842cb7ec1c3f62fca75141
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-12 12:22:34 +0530

    Feature: Update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 import Post from "./Post";
 
 export default function Header() {
   return (
     <header className="container h-16 flex items-center">
       <div className="px-4 w-full grid grid-cols-2 gap-4">
-        <div className="flex items-center gap-4">
+        <Link to="/" className="flex items-center gap-4">
           <img
             src="/images/recruiterflow.png"
             alt="recruiterflow logo"
             className="h-6"
           />
           <p className="text-xl font-bold">News</p>
-        </div>
+        </Link>
 
         <div className="place-self-end flex items-center gap-4">
           <Post />
