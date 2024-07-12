831144822ca39b850106bfedd89b3714907b4e28
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-12 12:21:49 +0530

    Feature: Add Preloader on World Business Section

--- src/routes/Home/WorldBusiness/index.tsx
+++ src/routes/Home/WorldBusiness/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { useGetBusinessNewsQuery } from "@/services/newsApi";
-import { Skeleton } from "@/components/ui/Skeleton";
 import Article from "./_components/Article";
+import Preloader from "@/components/ui/Preloader";
 
 export default function WorldBusiness() {
   const { data, isLoading } = useGetBusinessNewsQuery();
   const articles = React.useMemo(() => (data ? data.results : []), [data]);
 
-  const skeletonArray = Array.from({ length: 20 }, (_, index) => (
-    <Skeleton key={index} className="flex-1 w-full" />
-  ));
-
+  if (isLoading) return <Preloader />;
   return (
     <section>
       <fieldset className="px-4 pb-12 space-y-4 border rounded-lg">
@@ -19,7 +16,6 @@ export default function WorldBusiness() {
         </legend>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {isLoading && skeletonArray}
           {articles.map((article) => (
             <Article key={article.slug_name} article={article} />
           ))}
