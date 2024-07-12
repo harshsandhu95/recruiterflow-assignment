c4aca05abcad0cbb22111ef6765c9d5a6a322e47
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-12 12:22:26 +0530

    API: Create API Route to get business news

--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -17,7 +17,14 @@ export const newsApi = createApi({
     getBusinessNews: build.query<BusinessNewsResponse, void>({
       query: () => `content/nyt/business.json?api-key=${apiKey}`,
     }),
+    getSingleBusinessNews: build.query<BusinessNews | undefined, string>({
+      query: () => `content/nyt/business.json?api-key=${apiKey}`,
+      transformResponse(response: BusinessNewsResponse, _meta, arg) {
+        return response.results.find((news) => news.slug_name === arg);
+      },
+    }),
   }),
 });
 
-export const { useGetBusinessNewsQuery } = newsApi;
+export const { useGetBusinessNewsQuery, useGetSingleBusinessNewsQuery } =
+  newsApi;
